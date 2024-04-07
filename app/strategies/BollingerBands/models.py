84b038c30a844c1b20f6f7a54d2cfb442df9ba76
Lomowg <sokol789123@yandex.ru>
2024-04-07 23:14:35 +0300

    Added check_interval parameter

--- app/strategies/BollingerBands/models.py
+++ app/strategies/BollingerBands/models.py
@@ -8,11 +8,13 @@ class BollingerBandsConfig(BaseModel):
     days_back_to_consider: The number of days back to consider in calculation
     num_std: The number of std to define the width of the Bollinger bands
     quantity_limit: The maximum quantity of the instrument to have in the portfolio
+    check_interval: The interval in seconds to check for a new prices and for interval recalculation
     """
 
     days_back_to_consider: int = Field(30, g=0)
     num_std: int = Field(2, ge=1, le=3)
     quantity_limit: int = Field(0, ge=0)
+    check_interval: int = Field(60, g=0)
 
 
 class Corridor(BaseModel):
