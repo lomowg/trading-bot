6db5ed83389a881bb664a14ae2b09dfebd817457
Lomowg <sokol789123@yandex.ru>
2024-04-07 23:10:30 +0300

    Added quantity_limit parameter

--- app/strategies/BollingerBands/models.py
+++ app/strategies/BollingerBands/models.py
@@ -7,10 +7,12 @@ class BollingerBandsConfig(BaseModel):
 
     days_back_to_consider: The number of days back to consider in calculation
     num_std: The number of std to define the width of the Bollinger bands
+    quantity_limit: The maximum quantity of the instrument to have in the portfolio
     """
 
     days_back_to_consider: int = Field(30, g=0)
     num_std: int = Field(2, ge=1, le=3)
+    quantity_limit: int = Field(0, ge=0)
 
 
 class Corridor(BaseModel):
