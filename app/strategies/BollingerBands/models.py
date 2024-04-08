51006f4bc150f825aaa4cbed8b202ed57781663b
Lomowg <sokol789123@yandex.ru>
2024-04-08 18:58:50 +0300

    Changed days_back_to_considering parameter to correct length parameter

--- app/strategies/BollingerBands/models.py
+++ app/strategies/BollingerBands/models.py
@@ -5,14 +5,14 @@ class BollingerBandsConfig(BaseModel):
     """
     Bollinger Bands Configuration
 
-    days_back_to_consider: The number of days back to consider in calculation
+    length: The time period to be used in calculating the SMA which creates the base for the Upper and Lower Bands
     num_std: The number of std to define the width of the Bollinger bands
     quantity_limit: The maximum quantity of the instrument to have in the portfolio
     check_interval: The interval in seconds to check for a new prices and for interval recalculation
     stop_loss_percent: The percent from the price to trigger a stop loss
     """
 
-    days_back_to_consider: int = Field(30, g=0)
+    length: int = Field(20, g=0)
     num_std: int = Field(2, ge=1, le=3)
     quantity_limit: int = Field(0, ge=0)
     check_interval: int = Field(60, g=0)
